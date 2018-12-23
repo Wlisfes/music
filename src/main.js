2e3487254eed0d15ff55490de36e42cf0b0c0a9f
情雨随风 <876451336@qq.com>
2018-12-23 17:03:38 +0800

    新增vuex

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import store from './store'
 import router from './router'
 import * as api from './api/api'
 import * as status from './api/apiConfig'
@@ -38,6 +39,7 @@ Vue.prototype.back = () => {
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
---
+++ src/store/state.js
@@ -0,0 +1,3 @@
+export default {
+    BG_URL: '0'
+}
